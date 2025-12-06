517ff81bfd6e619a566dc5543277961c38e4c04b
dayastan <dayastant@gmail.com>
2025-12-06 21:41:54 +0530

    I am add shandcn and dark and light mode themeprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-blue-500 font-bold">Home</h1>
-   </div>
+    <div className="p-5">
+      <div className="w-full flex justify-end">
+        <ThemeToggle/>
+      </div>
+      <h1 className="text-blue-500 fon-bold ">Home</h1>
+      <h1 className="text-blue-500 fon-bold font-barlow">Home</h1>
+      <Button>Daya</Button>
+    </div>
   );
 }
