3ecd6f348fe98cd769e9a47d6152059249902370
dayastan <dayastant@gmail.com>
2025-12-12 23:10:29 +0530

    Category form create now form validation add now

--- src/components/dashboard/forms/category-details.tsx
+++ src/components/dashboard/forms/category-details.tsx
@@ -1,19 +1,95 @@
-import {Category} from "@/generated/prisma/client"
+"use client"
+import { Category } from "@/generated/prisma/client"
 import { CategoryFormSchema } from "@/lib/schemas";
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
 import * as z from "zod";
+import { toast } from "sonner";
+import { Value } from "@radix-ui/react-select";
+import { AlertDialog } from "@/components/ui/alert-dialog";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-interface CategoryDetailsProps{
-    data?:Category
+interface CategoryDetailsProps {
+    data?: Category
 }
 
-const CategoryDetails : FC<CategoryDetailsProps> =({data}) =>{
+const CategoryDetails: FC<CategoryDetailsProps> = ({ data }) => {
 
-    const form = useForm<z.infer<typeof CategoryFormSchema>>
-    return <div>
-    </div>
-}
+    const form = useForm<z.infer<typeof CategoryFormSchema>>({
+        mode: "onChange",
+        resolver: zodResolver(CategoryFormSchema),
+        defaultValues: {
+            name: data?.name,
+            image: data?.image ? [{ url: data?.image }] : [],
+            url: data?.url,
+            featured: data?.featured,
+
+        },
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    useEffect(() => {
+        if (data) {
+            form.reset({
+                name: data?.name,
+                image: [{ url: data?.image }],
+                url: data?.url,
+                featured: data?.featured,
+            });
+        }
+    }, [data, form, toast])
+
+
+
+    const handleSubmit = async (values: z.infer<typeof CategoryFormSchema>) => {
+        console.log(values)
+    }
+    return <AlertDialog>
+        <Card className="w-full">
+            <CardHeader>
+                <CardTitle> Category information</CardTitle>
+                <CardDescription>{data?.id ? `Update ${data?.name} category information.`:" Lets create a category. You can edit category later from the categories table or the category page."}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+                        <FormField 
+                        disabled={isLoading}
+                        control={form.control}
+                        name="name"
+                        render={({field}) =>(
+                            <FormItem className="flex-1">
+                                <FormLabel>Category name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Name " {...field}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+                        <FormField 
+                        disabled={isLoading}
+                        control={form.control}
+                        name="url"
+                        render={({field}) =>(
+                            <FormItem className="flex-1">
+                                <FormLabel>Category url</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="/category-url" {...field}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    </AlertDialog>
+};
 
 export default CategoryDetails
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,13 +1,32 @@
+import { features } from "node:process";
 import * as z from "zod";
 
+
+
+// Catgeory form schema
 export const CategoryFormSchema = z.object({
-  ame: z
+  name: z
     .string()
-    .min(1, { message: "Category name is required." }) // replaces required_error
+    .min(1, { message: "Category name is required." })
     .min(2, { message: "Category name must be at least 2 characters long." })
     .max(50, { message: "Category name cannot exceed 50 characters." })
     .regex(/^[a-zA-Z0-9\s'&-]+$/, {
       message:
         "Only letters, numbers, and spaces are allowed in the category name.",
     }),
+  image: z
+    .object({
+      url: z.string(),
+    })
+    .array()
+    .length(1, "Choose a category image."),
+  url: z
+    .string()
+    .min(2, { message: "Category url must be at least 2 characters long." })
+    .max(50, { message: "Category url cannot exceed 50 characters." })
+    .regex(/^(?!.*(?:[-_ ]){2,})[a-zA-Z0-9_-]+$/, {
+      message:
+        "Only letters, numbers, hyphen, and underscore are allowed in the category url, and consecutive occurrences of hyphens, underscores, or spaces are not permitted.",
+    }),
+  featured: z.boolean().default(false).optional(),
 });
